7e4c626bff2fd7d1571f4efa02589a6dbd04aa3c
Mehdi <chihi.med@gmail.com>
2024-05-12 16:19:41 +0200

    adding id to user

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema(
     {
+        id: {
+            type: Number,
+        },
+        
         username: {
             type: String,
             required: [true, "please enter a username"],
